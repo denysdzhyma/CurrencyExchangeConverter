7320a4dab1fd62f5cd4bf17aeee348d6651b6d0d
Denys <dennisjim1600@gmail.com>
2024-12-23 22:13:20 -0500

    Updated file
    No descriptions.

--- Currency Exchange Converter/CurrencyOptions.swift
+++ Currency Exchange Converter/CurrencyOptions.swift
@@ -5,7 +5,21 @@
 //  Created by Denys on 12/21/24.
 //
 
-
+func currencyConverter(amount: Double, rates: CurrencyRates, options: Int) {
+    switch options {
+        case 1:
+            let total = amount * rates.EUR
+            print("Total in EUR: \(total)")
+        case 2:
+            let total = amount * rates.GBP
+            print("Total in GBP: \(total)")
+        case 3:
+            let total = amount * rates.CAD
+            print("Total in CAD: \(total)")
+        default:
+            print("ERROR (300): Read the documentation list errors.")
+    }
+}
 
 
 
--- Currency Exchange Converter/main.swift
+++ Currency Exchange Converter/main.swift
@@ -19,21 +19,7 @@ if let responseData = loadJSONFile(urlString: "https://raw.githubusercontent.com
     print("ERROR: Decoding JSON file failed.")
 }
 
-func currencyConverter(amount: Double, rates: CurrencyRates, options: Int) {
-    switch options {
-        case 1:
-            let total = amount * rates.EUR
-            print("Total in EUR: \(total)")
-        case 2:
-            let total = amount * rates.GBP
-            print("Total in GBP: \(total)")
-        case 3:
-            let total = amount * rates.CAD
-            print("Total in CAD: \(total)")
-        default:
-            print("ERROR (300): Read the documentation list errors.")
-    }
-}
+
 
 currencyOptions()
 
@@ -49,10 +35,4 @@ if let currencyOptionsStr = readLine(), let currencyOptions = Int(currencyOption
     } else {
     print("ERROR (302): Invalid option selected.")
 }
-
-
-
-
-
-
 
